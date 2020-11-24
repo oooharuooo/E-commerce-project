6594e7861187a3f0b024d4641de5eb762693a676
Dat <haruthong@gmail.com>
2020-11-23 17:20:32 -0800

    Added file for Homepage

--- e-commerce-shopping/src/App.js
+++ e-commerce-shopping/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Homepage from './Homepage'
 
 function App() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <Homepage />
   )
 }
 
---
+++ e-commerce-shopping/src/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Homepage.scss";
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
